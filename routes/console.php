97284594fc9ab5be1d1a82a927fff0402da51073
Claudio Varandas <cvarandas@gmail.com>
2025-12-26 13:43:37 +0000

    update scheduled command

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 // Schedule upcoming games update (daily at 2 AM)
-Schedule::command('igdb:upcoming:update --days=5')
+Schedule::command('igdb:upcoming:update --days=2')
     ->twiceDaily('07:00', '19:00')
     ->withoutOverlapping()
     ->onOneServer();
