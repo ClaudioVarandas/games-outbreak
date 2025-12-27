e0e69d9f0fa905d419150e073553b37f5dfefe01
Claudio Varandas <cvarandas@gmail.com>
2025-12-27 17:28:30 +0000

    fix: console scheduled job

--- routes/console.php
+++ routes/console.php
@@ -4,8 +4,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-// Schedule upcoming games update (daily at 2 AM)
+// Schedule upcoming games update (twice daily at 7 AM and 7 PM)
 Schedule::command('igdb:upcoming:update --days=2')
-    ->twiceDaily('07:00', '19:00')
+    ->twiceDaily(7, 19)
+    ->name('igdb-upcoming-update')
     ->withoutOverlapping()
     ->onOneServer();
