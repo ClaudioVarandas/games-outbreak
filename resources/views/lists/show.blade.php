e2ca01b547a9a66822d2754ab533d8a7cc7e75ec
Claudio Varandas <cvarandas@gmail.com>
2026-01-10 20:35:14 +0000

    revert og:image

--- resources/views/lists/show.blade.php
+++ resources/views/lists/show.blade.php
@@ -6,7 +6,7 @@
     {{-- Open Graph Meta Tags --}}
     <meta property="og:title" content="{{ $gameList->name }} | GamesOutbreak">
     <meta property="og:description" content="Browse {{ $gameList->games->count() }}+ games from {{ $gameList->name }}. Filter by platform, genre, and more.">
-    <meta property="og:image" content="bananas">
+    <meta property="og:image" content="{{ $gameList->og_image_url ?? asset('images/og-default.png') }}">
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:type" content="website">
 
